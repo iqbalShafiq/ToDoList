be2d62b9b2449437d5c7c584d0ca3421be22c8b6
Iqbal Shafiq Rozaan <iqbal.syafiq.rozaan@gmail.com>
2022-01-27 14:40:17 +0700

    Initial commit

---
+++ app/src/main/java/space/iqbalsyafiq/todolistbtsid/MainActivity.kt
@@ -0,0 +1,11 @@
+package space.iqbalsyafiq.todolistbtsid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
